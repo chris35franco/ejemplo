98bb3ca356ea8b0ac34cbf633bbda72baa9b3553
Christian Franco <cristiancamilofranco636@gmail.com>
2024-08-23 09:51:59 -0500

    practica

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PaginComponent } from "./pages/pagin/pagin.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PaginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ejemplo';
+
+  name:string = "";
+  email:string = "";
+
+  enviar(){
+    this.name = (<HTMLInputElement>document.getElementById("name")).value;
+    this.email = (<HTMLInputElement>document.getElementById("email")).value;
+
+    let nuevoItem = document.createElement("li");
+    nuevoItem.textContent = "Nombre: " + this.name + ", Email: " + this.email;
+
+    document.getElementById("formData")?.appendChild(nuevoItem);
+
+  }
+
 }
---
+++ src/app/pages/pagin/pagin.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pagin',
+  standalone: true,
+  imports: [],
+  templateUrl: './pagin.component.html',
+  styleUrl: './pagin.component.css'
+})
+export class PaginComponent {
+
+}
